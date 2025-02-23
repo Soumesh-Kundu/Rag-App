66cb2c6e2ce3361a0a0b80e1305fc4de45899611
Soumesh Kundu <iamsoumo26@gmail.com>
2025-02-23 18:59:55 +0530

    v2: Markdown upgraded from v8 to v10

--- global.d.ts
+++ global.d.ts
@@ -1,43 +1,26 @@
-import type { JSX as Jsx } from "react/jsx-runtime";
-
 declare global {
-  namespace JSX {
-    type ElementClass = Jsx.ElementClass;
-    type Element = Jsx.Element;
-    type IntrinsicElements = Jsx.IntrinsicElements;
+  declare module "react" {
+    namespace JSX {
+      interface IntrinsicElements {
+        "l-ring": React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLElement>,
+          HTMLElement
+        > & {
+          size?: number;
+          color?: string;
+          stroke?: number;
+          speed?: number;
+        };
+        "l-dot-wave": React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLElement>,
+          HTMLElement
+        > & {
+          size?: number;
+          color?: string;
+          stroke?: number;
+          speed?: number;
+        };
+      }
+    }
   }
 }
-
-declare global {
-    declare module "react" {
-      namespace JSX {
-        interface IntrinsicElements {
-          "l-ring": React.DetailedHTMLProps<
-            React.HTMLAttributes<HTMLElement>,
-            HTMLElement
-          > & {
-            size?: number;
-            color?: string;
-            stroke?: number;
-            speed?: number;
-          };
-          "l-dot-wave": React.DetailedHTMLProps<
-            React.HTMLAttributes<HTMLElement>,
-            HTMLElement
-          > & {
-            size?: number;
-            color?: string;
-            stroke?: number;
-            speed?: number;
-          }
-        }
-      }
-    }
-    declare module "react/jsx-runtime" {
-      namespace JSX {
-        interface IntrinsicElements {
-        }
-      }
-    }
-  }
-  
